043d6c2e40170ad96b2b374f4405b002b8d3dc30
lucas <lucascarlosrodex@gmail.com>
2024-11-06 19:48:03 -0300

    implementado

--- app/src/main/java/com/example/appsalvapets/Ong.java
+++ app/src/main/java/com/example/appsalvapets/Ong.java
@@ -1,4 +1,54 @@
 package com.example.appsalvapets;
 
-public class Ong {
+import java.io.Serializable;
+
+public class Ong  implements Serializable {
+
+    private long id;
+    private String nome;
+    private String endereco;
+    private String contato;
+
+    public Ong() {
+
+    }
+
+    public Ong(String nome, String endereco, String contato) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.contato = contato;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
 }
+
