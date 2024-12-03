4aa73938cc459e2ba3f8aec0efcba080d02e52e9
lucas <lucascarlosrodex@gmail.com>
2024-12-03 00:22:20 -0300

    Add botão login na tela Main Add tela login validando usuario inserido pelo Postman no banco

--- app/src/main/java/com/example/appsalvapets/Pet.java
+++ app/src/main/java/com/example/appsalvapets/Pet.java
@@ -1,4 +1,35 @@
 package com.example.appsalvapets;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
 public class Pet {
+    @SerializedName("id")
+    private long id;
+
+    @SerializedName("nome")
+    private String nome;
+
+    @SerializedName("raca")
+    private String raca;
+
+    @SerializedName("idade")
+    private int idade;
+
+    @SerializedName("imagem")
+    private String imagemBase64;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getRaca() { return raca; }
+    public void setRaca(String raca) { this.raca = raca; }
+
+    public int getIdade() { return idade; }
+    public void setIdade(int idade) { this.idade = idade; }
+
+    public String getImagemBase64() { return imagemBase64; }
+    public void setImagemBase64(String imagemBase64) { this.imagemBase64 = imagemBase64; }
 }
+
