52c95a5fb279df9ed51de089c229a7a10690a670
thiagocds01 <thiago.cds01@gmail.com>
2024-12-11 19:55:32 -0300

    Ajuste tela cadastro 3

--- app/src/main/java/com/example/appsalvapets/activity/CadastroActivity.java
+++ app/src/main/java/com/example/appsalvapets/activity/CadastroActivity.java
@@ -67,11 +67,8 @@ public class CadastroActivity extends AppCompatActivity implements View.OnClickL
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        txtSeleOng = findViewById(R.id.txtSeleOng);
         edtCadUsuario = findViewById(R.id.edtCadUsuario);
         edtCadSenha = findViewById(R.id.edtCadSenha);
-        spnTipoUsuario = findViewById(R.id.spnTipoUsuario);
-        spnSeleOng = findViewById(R.id.spnSeleOng);
         btnCadUsuario = findViewById(R.id.btnCadUsuario);
         btnCadUsuario.setOnClickListener(this);
         btnCadCancelar = findViewById(R.id.btnCadCancelar);
@@ -85,7 +82,7 @@ public class CadastroActivity extends AppCompatActivity implements View.OnClickL
 
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.100.74:80/")
+                .baseUrl("http://192.168.50.229:80/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
